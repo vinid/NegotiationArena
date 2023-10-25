2e53fa6f2e0bb5c82f97cbf29595acbb95465e7a
Patrick John Chia <patrickjohncyh@icloud.com>
2023-10-25 23:06:54 +0800

    Compute utility of each proposed trade

--- agents.py
+++ agents.py
@@ -1,22 +1,36 @@
 from prompts import structured_calls
 import openai
 import os
-from utils import Goal
+from utils import *
 import copy
 
 class Agent:
 
-    def __init__(self, potential_resources_txt, resources, goals: Goal, role):
+    def __init__(self, 
+                 potential_resources_txt,
+                 resources, 
+                 goals: Goal, 
+                 role):
         self.potential_resources_txt = potential_resources_txt
         self.inital_resources = copy.deepcopy(resources)
         self.resources = resources
         self.goals = goals
         self.role = role
+        # define marginal utility as proportional to distance from objective
+        # for now it is static since we only allow one trade
+        self.marginal_utility = (goals-resources)
 
     def prompt(self):
         return structured_calls.format(self.potential_resources_txt,
                                        self.resources.to_prompt(),
                                        self.goals.to_prompt(), self.role)
+    
+    def utility(self):
+        """
+        Computes the present utility of an agent
+        """
+
+
 
 class ChatGPTAgent(Agent):
 
