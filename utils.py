d8e245eb881ee28ff41246d97dcb968cb9306548
Patrick John Chia <patrickjohncyh@icloud.com>
2023-10-26 10:54:28 +0800

    fix more prompting bugs

--- utils.py
+++ utils.py
@@ -21,8 +21,7 @@ class Resources:
         return sum(self.resource_dict.values())
 
     def to_prompt(self):
-        # ignore brackets
-        return str(self)[1:-1]
+        return res = [f"{k}: {v}" for k, v in self.resource_dict.items()]
 
     def __eq__(self, other):
         return self.resource_dict == other.resource_dict
