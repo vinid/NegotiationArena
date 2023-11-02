752a928d0dcbd205aa92976a46b4b0af0c576e0c
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-02 16:41:31 +0800

    fix proposed trade formatting in prompt

--- control/prompts.py
+++ control/prompts.py
@@ -1,9 +1,10 @@
 structured_calls = """
-You are playing a strategic game of trading resources with another player whose resources you have no knowledge about.  
+You are playing a strategic game of trading resources with another player whose resources you have no knowledge about.
+Your main objective is to reach your goal.
 
 START OF THE RULES AND FORMATTING GUIDE.
 
-1) This is the last round of trading. So make sure to work your way up to the best possible trade.
+1) This is the FINAL round of trading. So make sure to work your way up to the best possible trade.
 Only the trade will affect your resources, there will be no other event affecting your resources.
 
 2) Player 1 will suggest an initial trade like this:
@@ -19,10 +20,10 @@ NEWLY PROPOSED TRADE: NONE
 
 or reject the trade and you must propose a new trade:
 MY RESPONSE: REJECTED
-NEWLY PROPOSED TRADE: Player 1 Gives: item1: amount, item2: amount, Player 2 Gives: item1: amount, item2: amount, ...
+NEWLY PROPOSED TRADE: Player 1 Gives item1: amount, item2: amount, Player 2 Gives item1: amount, item2: amount, ...
 
 Note: the game will end if one of the players accepts. 
-This means that you have to be careful about accepting a trade.  
+This means that you have to be careful about both accepting and proposing trade.  
 If you feel like you are ok with the resources, don't want to share any or you want to buy time before deciding you have you can just keep rejecting offers and offer to exchange 0 resources, that is the same as offering nothing.
 
 5) You can reason on why you are A) proposing, B) rejecting or C) accepting a trade with:
