65f2f11f59e2a60d649effd6d7c0b17195930a1d
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-16 21:11:54 +0800

    remove some debug

--- game/agents/agent_behaviours.py
+++ game/agents/agent_behaviours.py
@@ -5,10 +5,9 @@ from game.constants import REASONING_TAG
 class SelfCheckingAgent(Agent, ABC):
 
     def think(self):
-        
         # do one step of thinking
         super().think()
-        print("reflecting!")
+        # print("reflecting!")
         # prompt agent to check proposal
         self.update_conversation_tracking("system", "Check your proposal to make sure you can win the game with this proposal. If you cannot, propose a new trade else propose the same trade.")
         # think again
@@ -17,8 +16,7 @@ class SelfCheckingAgent(Agent, ABC):
 class ReasoningAgent(Agent, ABC):
 
     def think(self):
-        print("reasonning!")
-        
+        # print("reasoning!")
         # remind agent to think step by step
         self.update_conversation_tracking("system", f"Reason about your response step by step with <{REASONING_TAG}> [add reasoning] </{REASONING_TAG}>.")
         # think 
