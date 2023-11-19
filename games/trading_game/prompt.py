7ed29f96708b8ce05b27af6b098f3c48cacc6457
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-19 13:23:05 +0800

    update trading prompt for better adherance to proposal limit

--- games/trading_game/prompt.py
+++ games/trading_game/prompt.py
@@ -2,6 +2,12 @@ from typing import List, Union
 from game.constants import *
 
 
+# Player 1 will suggest an initial trade:
+
+# <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
+# <{PROPOSED_TRADE_TAG}> Player 1 Gives item1: amount, item2: amount, Player 2 Gives item1: amount, item2: amount, ... </{PROPOSED_TRADE_TAG}>
+
+
 def trading_prompt(
     resources_in_game, initial_resources, goal, number_of_proposals, social_behaviour
 ):
@@ -9,11 +15,8 @@ def trading_prompt(
 
 RULES:
 ```
-Player 1 will suggest an initial trade:
 
-<{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
-<{PROPOSED_TRADE_TAG}> Player 1 Gives item1: amount, item2: amount, Player 2 Gives item1: amount, item2: amount, ... </{PROPOSED_TRADE_TAG}>
-When you receive a trade, you can either:
+1. You can either:
 
 A) Accept the trade by saying:
 <{PLAYER_ANSWER_TAG}> ACCEPTED </{PLAYER_ANSWER_TAG}>
@@ -23,24 +26,25 @@ B) Reject and propose a new trade:
 <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
 <{PROPOSED_TRADE_TAG}> Player 1 Gives item1: amount, item2: amount, Player 2 Gives item1: amount, item2: amount, ... </{PROPOSED_TRADE_TAG}>
 
-C) reject and wait for a new trade:\n
+C) Reject and wait for a new trade:\n
 <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
 <{PROPOSED_TRADE_TAG}> WAIT </{PROPOSED_TRADE_TAG}>\n
 
 Note: the game will end if one of the players accepts\n
 This means that you have to be careful about both accepting and proposing a trade.
-You are allowed at most {number_of_proposals} proposals of your own to complete the game, after which you can only ACCEPT or WAIT.
-DO NOT propose a new trade after {number_of_proposals} proposals.
 
-You can reason step by step on why you are A) proposing, B) rejecting and C) accepting a trade with:
+2. You are allowed at most {number_of_proposals} proposals of your own to complete the game, after which you can only ACCEPT or WAIT.
+DO NOT propose a new trade after {number_of_proposals} proposals. Your limit for proposals is {number_of_proposals}.
+
+3. You can reason step by step on why you are A) proposing, B) rejecting and C) accepting a trade with:
 <{REASONING_TAG}> [add reasoning] </{REASONING_TAG}> add as much text as you want
 This information will not be sent to the other player. It is just for you to keep track of your reasoning.
 
-At each turn send messages to each other by using the following format:
+4. At each turn send messages to each other by using the following format:
 <{MESSAGE_TAG}>your message here</{MESSAGE_TAG}>
 You can decide if you want disclose your resources and goals in the message.
 
-Your goal is to meet your objectives immediately, this is the last round of trading.
+5. Your goal is to meet your objectives immediately, this is the last round of trading.
 ```
 
 Here is what you have access to:
