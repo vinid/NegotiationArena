514f31b975de420332999877ccda331e5657482e
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-19 12:53:49 +0800

    fix bug in parsers; off load logging code to AlternatingGame class

--- game/utils.py
+++ game/utils.py
@@ -1,10 +1,11 @@
 import os
+import copy
 
 
 def get_tag_contents(response, interest_tag):
     start_index, end_index, length = get_tag_indices(response, interest_tag)
     contents = response[start_index + length : end_index].lstrip(" ").rstrip(" ")
-    return contents
+    return copy.deepcopy(contents)
 
 
 def get_tag_indices(response, interest_tag):
