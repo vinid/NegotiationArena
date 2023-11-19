b3db7597ac659d031597c4ab48d33f7b4e5d122d
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-19 10:30:50 +0800

    prompt upate

--- games/trading_game/trading_prompts.py
+++ games/trading_game/trading_prompts.py
@@ -6,6 +6,9 @@ def trading_prompt(
     resources_in_game, initial_resources, goal, number_of_proposals, social_behaviour
 ):
     prompt = f"""You are playing a strategic game of trading resources with another player whose resources you have no knowledge about. 
+
+RULES:
+```
 Player 1 will suggest an initial trade:
 
 <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
@@ -19,12 +22,15 @@ A) Accept the trade by saying:
 B) Reject and propose a new trade:
 <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
 <{PROPOSED_TRADE_TAG}> Player 1 Gives item1: amount, item2: amount, Player 2 Gives item1: amount, item2: amount, ... </{PROPOSED_TRADE_TAG}>
+
 C) reject and wait for a new trade:\n
 <{PLAYER_ANSWER_TAG}> WAIT </{PLAYER_ANSWER_TAG}>
 <{PROPOSED_TRADE_TAG}> WAIT </{PROPOSED_TRADE_TAG}>\n
 
 Note: the game will end if one of the players accepts\n
 This means that you have to be careful about both accepting and proposing a trade.
+You are allowed at most {number_of_proposals} proposals of your own to complete the game. After which you can only ACCEPT or WAIT.
+You MUST NOT propose a new trade after {number_of_proposals} proposals.
 
 You can reason step by step on why you are A) proposing, B) rejecting and C) accepting a trade with:
 <{REASONING_TAG}> [add reasoning] </{REASONING_TAG}> add as much text as you want
@@ -35,12 +41,14 @@ At each turn send messages to each other by using the following format:
 You can decide if you want disclose your resources and goals in the message.
 
 Your goal is to meet your objectives immediately, this is the last round of trading.
+```
 
 Here is what you have access to:
+```
 Resources available in the game: {resources_in_game}
 <{RESOURCES_TAG}> {initial_resources} </{RESOURCES_TAG}>
 <{GOALS_TAG}> {goal} </{GOALS_TAG}>
-
+```
 
 All the responses you send should contain the following and in this order:
 
@@ -59,7 +67,7 @@ Note, if you get less of each resource of your goal, you lose.
 More resources in general are always better.,
 You should win the game immediately
 
-You are allowed at most {number_of_proposals} proposals of your own to complete the game.
+
 
 {social_behaviour}
 """
