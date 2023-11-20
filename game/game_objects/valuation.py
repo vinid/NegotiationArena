42261d023a773fdfbee2cb394e440c8d6a19b2aa
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-20 10:57:46 +0800

    fix val obj

--- game/game_objects/valuation.py
+++ game/game_objects/valuation.py
@@ -10,7 +10,7 @@ from game.game_objects.resource import Resources
 @dataclass
 class Valuation:
     valuation_dict: dict = None
-    # e.g. {X:2, Y:4, M: 10} where 2 => 2M, 4 => 4M
+    # e.g. {X:2, Y:4,} where 2 => 2M, 4 => 4M
 
     def value(self, resources: Resources):
         val_of_resources = 0
@@ -25,15 +25,13 @@ class Valuation:
         res = [f"{k}: {v}" for k, v in self.valuation_dict.items()]
         return ", ".join(res)
 
+    def __str__(self):
+        res = [f"{k}: {v} {MONEY_TOKEN}" for k, v in self.valuation_dict.items()]
+        return ", ".join(res)
 
-def __str__(self):
-    res = [f"'{k}': {v}" for k, v in self.resource_dict.items()]
-    return "{" + ", ".join(res) + "}"
+    def __repr__(self):
+        res = [f"'{k}': {v}" for k, v in self.valuation_dict.items()]
+        return "{" + ", ".join(res) + "}"
 
-
-def __repr__(self):
-    return self.__str__(self)
-
-
-def json(self):
-    return self.valuation_dict
+    def json(self):
+        return self.valuation_dict
