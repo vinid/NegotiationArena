8f5d57962fa752e9a74775bb07638059c940bdec
Patrick John Chia <patrickjohncyh@icloud.com>
2023-11-29 12:40:06 +0800

    commiting for future use

--- runner/inverse_ultimatum.py
+++ runner/inverse_ultimatum.py
@@ -25,56 +25,56 @@ if __name__ == "__main__":
     num_iters = 20
     i = 0
     while i < num_iters:
-        # try:
-        a1 = ChatGPTAgent(
-            agent_name="Player 1",
-            model="gpt-4-1106-preview",
-        )
-        a2 = ChatGPTAgent(
-            agent_name="Player 2",
-            model="gpt-4-1106-preview",
-        )
-
-        c = UltimatumInverseGame(
-            iterations=2,
-            players=[a1, a2],
-            game_interface=UltimatumInverseGameInterface(),
-            resources_support_set=Resources({"x": 0}),
-            player_1_reject_resources=Resources({"x": 2}),
-            player_goals=[
-                UltimatumGoal(),
-                UltimatumGoal(),
-            ],
-            player_initial_resources=[
-                Resources({"x": 0}),
-                Resources({"x": 100}),
-            ],
-            player_social_behaviour=[
-                "You are completely rational and do not care about the other player.",
-                "You are completely rational and do not care about the other player.",
-            ],
-            player_roles=[
-                "You are Player 1",  # "You are Player 1, start by accepting or rejecting a future trade. You are self-interested. It is always better to have more resources",
-                "You are Player 2",  # "You are Player 2, start by making a proposal based on Player 1 decision.",
-            ],
-            log_dir="./.logs/inverse_ultimatum",
-        )
-        c.run()
-        i += 1
-
-        if c.game_state[-1]["summary"]["final_response"] == "OPTION_B":
-            num_accept += 1
-
-        print(
-            "\n====> EXPERIMENT ITERATION : {}; RUNNING ACCEPTANCE RATE: {} <====\n".format(
-                i, num_accept / i
+        try:
+            a1 = ChatGPTAgent(
+                agent_name="Player 1",
+                model="gpt-4-1106-preview",
             )
-        )
-    # except Exception as e:
-    #     print(
-    #         "WARNING: ITERATION <{0}> FAILED WITH ERROR: {1} \n RESTARTING ITERATION {0}".format(
-    #             i, e
-    #         )
-    #     )
+            a2 = ChatGPTAgent(
+                agent_name="Player 2",
+                model="gpt-4-1106-preview",
+            )
+
+            c = UltimatumInverseGame(
+                iterations=2,
+                players=[a1, a2],
+                game_interface=UltimatumInverseGameInterface(),
+                resources_support_set=Resources({"x": 0}),
+                player_1_reject_resources=Resources({"x": 2}),
+                player_goals=[
+                    UltimatumGoal(),
+                    UltimatumGoal(),
+                ],
+                player_initial_resources=[
+                    Resources({"x": 0}),
+                    Resources({"x": 100}),
+                ],
+                player_social_behaviour=[
+                    "This is a competetive game.",  # Player 2 is completely rational. You are completely self-interested and care only about your resources.",  # "You are completely rational and do not care about the other player.",
+                    "This is a competetive game.",  # "You are completely rational and do not care about the other player.",
+                ],
+                player_roles=[
+                    "You are Player 1.",  # "You are Player 1, start by accepting or rejecting a future trade. You are self-interested. It is always better to have more resources",
+                    "You are Player 2.",  # "You are Player 2, start by making a proposal based on Player 1 decision.",
+                ],
+                log_dir="./.logs/inverse_ultimatum",
+            )
+            c.run()
+            i += 1
+
+            if c.game_state[-1]["summary"]["final_response"] == "OPTION_B":
+                num_accept += 1
+
+            print(
+                "\n====> EXPERIMENT ITERATION : {}; RUNNING ACCEPTANCE RATE: {} <====\n".format(
+                    i, num_accept / i
+                )
+            )
+        except Exception as e:
+            print(
+                "WARNING: ITERATION <{0}> FAILED WITH ERROR: {1} \n RESTARTING ITERATION {0}".format(
+                    i, e
+                )
+            )
 
     print("\n====> FINAL ACCEPTANCE RATE: {} <====\n".format(num_accept / num_iters))
