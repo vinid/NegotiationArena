7d319a0325568bb47a6d105957dfd5572e837d63
vinid <chiccobia@gmail.com>
2023-12-03 18:33:10 -0800

    update

--- experiments/section_one/experiment_trading_maximization.py
+++ experiments/section_one/experiment_trading_maximization.py
@@ -12,7 +12,7 @@ from ratbench.utils import factory_agent
 
 load_dotenv("../.env")
 
-NUMBER_OF_FIGHTS = 30
+NUMBER_OF_FIGHTS = 15
 
 PAIRS_OF_AGENTS = ["gpt-4", "gpt-3.5", "claude-2", "claude-2.1"], ["gpt-4", "gpt-3.5", "claude-2", "claude-2.1"]
 PAIRS_OF_AGENTS = [(x, y) for x, y in itertools.product(*PAIRS_OF_AGENTS) if x != y]
@@ -21,7 +21,8 @@ if __name__ == "__main__":
 
     for agent1, agent2 in PAIRS_OF_AGENTS:
 
-        for i in range(NUMBER_OF_FIGHTS):
+        counter = 0
+        while counter < NUMBER_OF_FIGHTS:
             try:
                 a1 = factory_agent(agent1, agent_name=AGENT_ONE)
                 a2 = factory_agent(agent2, agent_name=AGENT_TWO)
@@ -50,6 +51,7 @@ if __name__ == "__main__":
                 )
 
                 c.run()
+                counter = counter + 1
             except Exception as e:
                 exception_type = type(e).__name__
                 exception_message = str(e)
